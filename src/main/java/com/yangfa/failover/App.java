17813ef7612a62969fc61d7b5b007c9678f9917f
yangfan <yangfan0720vip@163.com>
2017-03-16 00:04:50 +0800

    Initial commit

---
+++ src/main/java/com/yangfa/failover/App.java
@@ -0,0 +1,13 @@
+package com.yangfa.failover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
